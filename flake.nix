0d1f70b11eb935924a736e8d4843df003d629614
Faarid <173501018+faaridf@users.noreply.github.com>
2026-06-21 21:05:37 +0500

    home manager 26.05

--- flake.nix
+++ flake.nix
@@ -4,7 +4,8 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager/master";
+      # url = "github:nix-community/home-manager/master";
+      url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
