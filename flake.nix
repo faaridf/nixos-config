5a73e240e39d80ccbc5ac93a24c33059c0e1fd04
niko <173501018+faaridf@users.noreply.github.com>
2026-06-21 19:00:14 +0500

    reformatted again

--- flake.nix
+++ flake.nix
@@ -1,28 +1,35 @@
 {
-	description = "NixOS from Scratch";
-	inputs = {
-		nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-		home-manager = {
-			url = "github:nix-community/home-manager/master";
-			inputs.nixpkgs.follows = "nixpkgs";
-		};	
-	};
+  description = "NixOS from Scratch";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 
-	outputs = {self, nixpkgs, home-manager, ...}: {
-		nixosConfigurations.nixos-btw = nixpkgs.lib.nixosSystem {
-			system = "x86_64-linux";
-			modules = [
-				./configuration.nix	
-				home-manager.nixosModules.home-manager
-				{
-					home-manager = {
-						useGlobalPkgs = true;
-						useUserPackages = true;
-						users.niko = import ./home.nix;
-						backupFileExtension = "backup";
-					};
-				}
-			];
-		};
-	};
+  outputs =
+    {
+      self,
+      nixpkgs,
+      home-manager,
+      ...
+    }:
+    {
+      nixosConfigurations.nixos-btw = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              users.niko = import ./home.nix;
+              backupFileExtension = "backup";
+            };
+          }
+        ];
+      };
+    };
 }
--- home.nix
+++ home.nix
@@ -1,52 +1,51 @@
-	{ config, pkgs, ... }:
+{ config, pkgs, ... }:
 
 {
-	home.username = "niko";
-	home.homeDirectory = "/home/niko";
-	programs.git = {
+  home.username = "niko";
+  home.homeDirectory = "/home/niko";
+  programs.git = {
     enable = true;
     userName = "niko";
     userEmail = "173501018+faaridf@users.noreply.github.com";
   };
-	home.stateVersion = "26.05";
+  home.stateVersion = "26.05";
   #home.stateVersion = 25.11;
 
   home.sessionVariables = {
     NH_FLAKE = "/home/niko/nixos-dots";
   };
 
-	programs.bash = {
-		enable = true;
-		shellAliases = {
-			cf = "clear && fastfetch";
-			gg = "echo wow, so cool";
-		};
-		#profileExtra = ''
-		#	if [ -z "$WAYLAND_DISPLAY" ] && [ "$XDG_VTNR" = 1 ]; then
-		#		exec uwsm start select hyprland-uwsm.desktop
-		#	fi
-		#'';
-	};
-	home.file.".config/qtile".source = ./config/qtile;
-	home.file.".config/nvim".source = ./config/nvim;
+  programs.bash = {
+    enable = true;
+    shellAliases = {
+      cf = "clear && fastfetch";
+      gg = "echo wow, so cool";
+    };
+    #profileExtra = ''
+    #	if [ -z "$WAYLAND_DISPLAY" ] && [ "$XDG_VTNR" = 1 ]; then
+    #		exec uwsm start select hyprland-uwsm.desktop
+    #	fi
+    #'';
+  };
+  home.file.".config/qtile".source = ./config/qtile;
+  home.file.".config/nvim".source = ./config/nvim;
 
-
-	home.packages = with pkgs; [
-		neovim
-		ripgrep
-		nil
-		nixpkgs-fmt
-		nodejs
-		gcc
-		nh
-		nvd
-		nix-output-monitor
-		tealdeer
-		mpvScripts.mpris
-		mpvScripts.thumbfast
-		mpvScripts.mpv-webm
-		mpvScripts.webtorrent-mpv-hook
-		ladybird
-		wl-clipboard
-	];
+  home.packages = with pkgs; [
+    neovim
+    ripgrep
+    nil
+    nixpkgs-fmt
+    nodejs
+    gcc
+    nh
+    nvd
+    nix-output-monitor
+    tealdeer
+    mpvScripts.mpris
+    mpvScripts.thumbfast
+    mpvScripts.mpv-webm
+    mpvScripts.webtorrent-mpv-hook
+    ladybird
+    wl-clipboard
+  ];
 }
